5846479a0d39d8c54cb89d9195b06004bd5db204
skoropadas <alex@skoropad.dev>
2023-04-01 03:24:28 +0200

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
-import {NgDocModule} from '@ng-doc/app';
-import {NG_DOC_ROUTING, NgDocGeneratedModule} from '@ng-doc/generated';
-import {RouterModule} from '@angular/router';
-import {NgDocSidebarModule} from '@ng-doc/app/components/sidebar';
-import {NgDocNavbarModule} from '@ng-doc/app/components/navbar';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppComponent} from './app.component';
+import {
+  NgDocDefaultSearchEngine,
+  NgDocModule,
+  provideSearchEngine,
+} from '@ng-doc/app';
+import { NG_DOC_ROUTING, NgDocGeneratedModule } from '@ng-doc/generated';
+import { RouterModule } from '@angular/router';
+import { NgDocSidebarModule } from '@ng-doc/app/components/sidebar';
+import { NgDocNavbarModule } from '@ng-doc/app/components/navbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,18 +19,21 @@ import {AppComponent} from './app.component';
     BrowserAnimationsModule,
     NgDocNavbarModule,
     NgDocSidebarModule,
-    RouterModule.forRoot([
-      { path: '', redirectTo: 'button', pathMatch: 'full' },
-      ...NG_DOC_ROUTING,
-    ], {
-      scrollPositionRestoration: 'enabled',
-      anchorScrolling: 'enabled',
-      scrollOffset: [0, 70],
-    },),
+    RouterModule.forRoot(
+      [
+        { path: '', redirectTo: 'button', pathMatch: 'full' },
+        ...NG_DOC_ROUTING,
+      ],
+      {
+        scrollPositionRestoration: 'enabled',
+        anchorScrolling: 'enabled',
+        scrollOffset: [0, 70],
+      }
+    ),
     NgDocModule.forRoot(),
     NgDocGeneratedModule.forRoot(),
   ],
-  providers: [],
+  providers: [provideSearchEngine(NgDocDefaultSearchEngine)],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
