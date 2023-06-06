3c96bb76887aa0199e80f1333362ae794ce0cc70
Alex Skoropad <alex@skoropad.dev>
2023-06-06 12:48:09 +0200

    feat(project): update demo

--- src/app/docs/button/demos/button-demo.component.ts
+++ src/app/docs/button/demos/button-demo.component.ts
@@ -1,10 +1,13 @@
 import {Component} from '@angular/core';
+import {ButtonComponent} from "../../../code/button/button.component";
 
 @Component({
   selector: 'app-button-demo',
   template: `
     <button app-button (click)="notify()">My button</button>
   `,
+  standalone: true,
+  imports: [ButtonComponent],
 })
 export class ButtonDemoComponent {
   notify(): void {
