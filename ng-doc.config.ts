d0a4752f987be5dcc4cc0bbc0e257bdd2febd2f1
Alex Skoropad <alex@skoropad.dev>
2023-06-09 18:09:51 +0200

    Revert "feat(project): update demo"
    This reverts commit 430d320a999ea15292428f6d3599fe8c1891d577.

--- ng-doc.config.ts
+++ ng-doc.config.ts
@@ -1,7 +1,11 @@
 import {NgDocConfiguration} from "@ng-doc/builder";
+import {ngKeywordsLoader} from "@ng-doc/keywords-loaders";
 
 const config: NgDocConfiguration = {
-  cache: true
+  cache: true,
+  keywords: {
+    loaders: [ngKeywordsLoader()]
+  }
 }
 
 export default config;
